6595cc29ae7915fdabec3cdd262b26e007d3d7fe
Alex Lepauvre <alexander.lepauvre@ad.aesthetics.mpg.de>
2024-01-19 14:51:54 +0100

    Set the root folders directory in a separate folder

---
+++ environment_variables.py
@@ -0,0 +1,4 @@
+
+# Directories parameters
+bids_root = r"C:\Users\alexander.lepauvre\Documents\GitHub\iEEG-data-release\data_release\bids"
+fs_directory = r"C:\Users\alexander.lepauvre\Documents\GitHub\iEEG-data-release\data_release\bids\derivatives\fs"
