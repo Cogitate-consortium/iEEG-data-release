9a124552ee3afe1d4bb36e7729eacccaf0c4357a
Alex Lepauvre <alex.lepauvre@ae.mpg.de>
2024-06-07 16:36:13 +0200

    BF

--- environment_variables.py
+++ environment_variables.py
@@ -1,5 +1,3 @@
-
 # Directories parameters
-raw_root = r"C:\Users\alexander.lepauvre\Documents\GitHub\bayesian_evidence_accumulation\bids"
-bids_root = r"C:\Users\alexander.lepauvre\Documents\GitHub\bayesian_evidence_accumulation\bids"
-fs_directory = r"C:\Users\alexander.lepauvre\Documents\GitHub\bayesian_evidence_accumulation\bids\derivatives\fs"
+bids_root = r"C:\Users\alexander.lepauvre\Documents\GitHub\iEEG-data-release\bids-curate"
+fs_directory = r"C:\Users\alexander.lepauvre\Documents\GitHub\iEEG-data-release\bids-curate\derivatives\fs"
