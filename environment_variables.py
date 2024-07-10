38c44febeb0d6cf1d503e0e1127ca586fa4f97fd
Alex Lepauvre <alex.lepauvre@ae.mpg.de>
2024-07-10 13:57:47 +0200

    Update environment_variables.py

--- environment_variables.py
+++ environment_variables.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 # Parameters to adjust: 
 bids_root = r"C:\Users\alexander.lepauvre\Documents\GitHub\iEEG-data-release\bids-xnat"
-fs_directory = Path(bids_root, 'derivatives', 'fs')
+fs_directory = Path(bids_root, 'derivatives', 'fs') # Change only if you have your freesurfer root elsewhere
 
 # ===============================================================
 # Don't touch
