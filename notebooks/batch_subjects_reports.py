82d43e11bda3e950a36ba7a854ab1a60de09f2f0
Alex lepauvre <alex.lepauvre@ae.mpg.de>
2024-08-20 18:51:20 +0200

    Fix batch subjects dir

--- notebooks/batch_subjects_reports.py
+++ notebooks/batch_subjects_reports.py
@@ -23,17 +23,21 @@ def copy_images(src, dst):
 
 
 def subject_report_html(subject_id):
-    input_nb = 'ieeg-single-subject-report.ipynb'  # Replace with your notebook filename
-    output_nb = f'output_{subject_id}.ipynb'
-    report_dir = '../subjects_reports'
+    # Get the current file directory:
+    file_root = Path(__file__).resolve().parent
+    # Set paths:
+    input_nb = Path(file_root, 'ieeg-single-subject-report.ipynb') 
+    output_nb = Path(file_root, f'output_{subject_id}.ipynb')
+    report_dir = Path(file_root.parent, "subjects_reports")
     html_output = os.path.join(report_dir, f'report_{subject_id}.html')
+    img_src = Path(__file__).resolve().parent
 
     # Ensure the reports directory exists
     if not os.path.exists(report_dir):
         os.makedirs(report_dir)
 
     # Copy the images to the directory to make sure the notebook headers and footers don't break
-    copy_images('../img', os.path.join(report_dir, 'img'))
+    copy_images(img_src, os.path.join(report_dir, 'img'))
     # Execute the notebook for the given subject
     pm.execute_notebook(
         input_nb,
@@ -58,11 +62,14 @@ def subject_report_html(subject_id):
 if __name__ == "__main__":
     subjects = pd.read_csv(Path(ev.bids_root, "participants.tsv"), sep='\t')["participant_id"].to_list()
     subjects = ["CF102", "CE107"]
+    import os
+
     # Download the data if necessary:
     xnat_download(['sub-' + sub for sub in subjects], overwrite=False)
     for subject in subjects:
         print(subject)
-        try:
-            subject_report_html(subject)
-        except:
-            print("WARNING: The notebook could not be generated for sub-" + subject)
+        subject_report_html(subject)
+        # try:
+        #     subject_report_html(subject)
+        # except:
+        #     print("WARNING: The notebook could not be generated for sub-" + subject)
