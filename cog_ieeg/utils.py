af9e4db37cbf23c7123ff07335630ca25abdf919
AlexLepauvre <alex.lepauvre@ae.mpg.de>
2024-08-28 17:18:32 +0200

    Update utils.py
    Cleaner handling of default path and changes settings for end users

--- cog_ieeg/utils.py
+++ cog_ieeg/utils.py
@@ -16,19 +16,142 @@ This code is licensed under the MIT License.
 
 import json
 import os
+import platform
 from pathlib import Path
 import importlib.resources as pkg_resources
 
+_KNOWN_PIPELINES = ('preprocessing', 'onset_responsiveness', 'decoding')
 
-def get_dft_config(filename):
+
+def get_config_path(config_name):
+    with pkg_resources.path('cog_ieeg.config', config_name) as config_path:
+        return config_path
+
+
+def load_config(config_file):
+    with open(config_file, 'r') as configfile:
+        return json.load(configfile)
+
+
+def save_config(config, config_file):
+    with open(config_file, 'w') as configfile:
+        json.dump(config, configfile, indent=4)
+    return None
+
+
+def print_config(config):
+    # Print the content of the configuration in a nicely formatted way
+    print("Configuration content:\n")
+    print(json.dumps(config, indent=4))
+    return None
+
+
+def print_config_path(config_file):
+    # Ensure the config_file is a Path object for consistent formatting
+    config_file = Path(config_file)
+    # Print the full file path
+    print(f"Configuration file path: {config_file.resolve()}\n")
+    return None
+
+
+def get_data_directory():
+    system = platform.system()
+    
+    if system == "Windows":
+        data_dir = Path(os.getenv('USERPROFILE')) / "Documents" / "COGITATE"
+    elif system == "Darwin":  # macOS
+        data_dir = Path.home() / "Library" / "Documents" / "COGITATE"
+    else:  # Assume Linux or other UNIX-like systems
+        data_dir = Path.home() / ".local" / "share"
+
+    # Create the directory if it doesn't exist
+    data_dir.mkdir(parents=True, exist_ok=True)
+    
+    return data_dir
+
+
+def create_default_config():
+    # Define the default values
+    data_dir = get_data_directory()
+    bids_root = data_dir / 'bids'
+    
+    config = {
+        "Paths": {
+            "bids_root": str(bids_root),
+            "fs_directory": str(bids_root / 'derivatives' / 'fs')
+        },
+        "XNAT": {
+            "xnat_host": 'cogitate-data',
+            "xnat_project": 'COG_IEEG_EXP1_BIDS'
+        }
+    }
+    # Save the configuration to a JSON file
+    with open(get_config_path('config-path.json'), 'w') as configfile:
+        json.dump(config, configfile, indent=4)
+
+    return None
+
+
+def set_bids_root(new_path, update_fs_dir=True):
+    with open(get_config_path('config-path.json'), 'r') as configfile:
+        config = json.load(configfile)
+    config['Paths']['bids_root'] = str(new_path)
+    if update_fs_dir:
+        config['Paths']['fs_directory'] = str(Path(new_path) / 'derivatives' / 'fs')
+    # Save the configuration to a JSON file
+    save_config(config, get_config_path('config-path.json'))
+    return None
+
+
+def set_xnat_host(xnat_host):
+    with open(get_config_path('config-path.json'), 'r') as configfile:
+        config = json.load(configfile)
+    config['XNAT']['xnat_host'] = xnat_host
+    save_config(config, get_config_path('config-path.json'))
+    return None
+
+
+def set_xnat_project(xnat_project):
+    with open(get_config_path('config-path.json'), 'r') as configfile:
+        config = json.load(configfile)
+    config['XNAT']['xnat_project'] = xnat_project
+    save_config(config, get_config_path('config-path.json'))
+    return None
+
+
+def get_bids_root():
+    with open(get_config_path('config-path.json'), 'r') as configfile:
+        config = json.load(configfile)
+    return config['Paths']['bids_root']
+
+
+def get_xnat_host():
+    with open(get_config_path('config-path.json'), 'r') as configfile:
+        config = json.load(configfile)
+    return config['XNAT']['xnat_host']
+
+
+def get_xnat_project():
+    with open(get_config_path('config-path.json'), 'r') as configfile:
+        config = json.load(configfile)
+    return config['XNAT']['xnat_project']
+
+
+def get_pipeline_config(pipeline, preload=True):
     """
     Load a default configuration file from the 'config' directory within the package.
 
-    :param filename: The name of the configuration file (e.g., 'preprocessing_config-default.json').
+    :param pipeline: The name of the pipeline file (e.g., 'preprocessing_config-default.json').
+    :param preload: The name of the pipeline file (e.g., 'preprocessing_config-default.json').
     :return: Dictionary containing the configuration.
     """
-    with pkg_resources.path('cog_ieeg.config', filename) as config_path:
-        return Path(config_path)
+    assert pipeline.lower() in _KNOWN_PIPELINES, (f'pipeline must be {_KNOWN_PIPELINES}, \n'
+                                                  f'Got {pipeline} instead')
+    config = get_config_path(f'{pipeline.lower()}_config-default.json')
+    if preload:
+        return load_config(config)
+    else:
+        return config
 
 
 def path_generator(directory):
